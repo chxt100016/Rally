07a5c078e3d28cbb8abef0e7ef6c2552cad9b660
陈心桐 <chenxintong@pxb7.com>
2026-06-23 11:29:30 +0800

    source

--- rally-adapter/src/main/java/com/rally/web/tennis/TennisMatchController.java
+++ rally-adapter/src/main/java/com/rally/web/tennis/TennisMatchController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tennis/matches")
+@RequestMapping("/tennis/match")
 public class TennisMatchController {
 
     @Resource
