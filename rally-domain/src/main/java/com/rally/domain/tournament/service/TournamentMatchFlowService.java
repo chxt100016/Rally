5d20ee2db0f3719287bb6f952f71c9c9912c382e
chenxintong <chenxintong@pxb7.com>
2026-08-09 23:47:52 +0800

    deploy

--- rally-domain/src/main/java/com/rally/domain/tournament/service/TournamentMatchFlowService.java
+++ rally-domain/src/main/java/com/rally/domain/tournament/service/TournamentMatchFlowService.java
@@ -30,6 +30,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+
 public class TournamentMatchFlowService {
 
     private final TournamentMatchRepository matchRepository;
